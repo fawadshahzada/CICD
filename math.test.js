0ff4aea1960588061f6bc628fa17e70f060b1677
Fawad Shahzada <Fawad@clickaway.io>
2024-06-03 14:34:50 +0500

    testing workflow

--- math.test.js
+++ math.test.js
@@ -16,6 +16,21 @@ describe("Math.js tests", () => {
   it("Should test multiply", () => {
     expect(multiply(2, 2)).toBe(4);
   });
-  
+  it("should test  add  ", () => {
+    expect(add(2, 2)).toBe(4);
+  });
+
+  it("Should test subtract", () => {
+    expect(subtract(2, 2)).toBe(0);
+  });
+
+  it("Should test divide", () => {
+    expect(divide(2, 2)).toBe(1);
+  });
+
+  it("Should test multiply", () => {
+    expect(multiply(2, 2)).toBe(4);
+  });
+
   
 });
