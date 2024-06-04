2b248db7299520dcfe882202382db30036d5d542
Fawad Shahzada <Fawad@clickaway.io>
2024-06-04 15:01:51 +0500

    Code updates

--- math.test.js
+++ math.test.js
@@ -46,6 +46,13 @@ describe("Math.js tests", () => {
   it("Should test multiply", () => {
     expect(multiply(2, 2)).toBe(4);
   });
+  it("Should test divide", () => {
+    expect(divide(2, 2)).toBe(1);
+  });
+
+  it("Should test multiply", () => {
+    expect(multiply(2, 2)).toBe(4);
+  });
   
   
 });
